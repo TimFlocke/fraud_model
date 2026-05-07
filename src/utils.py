630d22ad5cc12a315f143c1bafbdee2b2f9cc3a6
Tim Flocke <timothy.flocke@outlook.com>
2026-05-07 11:57:17 -0400

    finished stage 2: EDA

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def full_value_counts(df, col):
@@ -9,3 +10,22 @@ def full_value_counts(df, col):
     b = df[col].value_counts(normalize=True).to_frame('pct')
 
     return a.merge(b, left_index=True, right_index=True)
+
+
+def get_relative_trigger(df, target, vars, upper_thresh, lower_thresh):
+    """
+    Create a table showing relative trigger rate for fraud
+    vs non fraud
+    """
+    
+    a = df.query(f"{target} == 1")[vars].mean().to_frame(target)
+    b = df.query(f"{target} == 0")[vars].mean().to_frame("not_" +target)
+
+    c = a.merge(b, left_index=True, right_index=True)
+    c['ix'] = np.round((c[target] / c["not_"+target])*100)
+
+    c['n_fraud'] = df.query(f"{target} == 1")[vars].sum().astype(int)
+    c['n_non_fraud'] = df.query(f"{target} == 0")[vars].sum().astype(int)
+
+    out = c[(c.ix < lower_thresh) | (c.ix > upper_thresh)]
+    return out.sort_values('ix', ascending = False)
